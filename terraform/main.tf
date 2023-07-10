e4cfafa8f31a0ab9e31a142c89c687f5f3f5b1c1
Amie Wei <amie.wei12@gmail.com>
2023-07-10 08:48:32 -0500

    update secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,8 @@ module "cloud-run" {
   location_name = var.location_name
   image         = var.image
   service_name  = var.service_name
+  mongo_pw      = var.mongo_pw
+  mongo_user    = var.mongo_user
 }
 
 module "cloud-scheduler" {
